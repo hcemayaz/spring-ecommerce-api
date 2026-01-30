81d6e305cb7e33ecefca11bf140a126a90740c1e
Cem Ayaz <cemayaz19@gmail.com>
2026-01-30 12:43:25 +0300

    Sonar changes and test updates.

--- src/test/java/com/example/springecommerceapi/SpringEcommerceApiApplicationTests.java
+++ src/test/java/com/example/springecommerceapi/SpringEcommerceApiApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.springecommerceapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.flyway.enabled=false"
+})
 class SpringEcommerceApiApplicationTests {
 
     @Test
